60dbd683a81a5d82428e644d8421401767b7f751
Dominic Tarr <dominic.tarr@gmail.com>
2014-06-05 19:17:12 +0000

    use infer-partial-order to show a representation of the partial order of the tests

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 'use strict';
 
 var RNG = require('rng')
+var path = require('path')
+var infer = require('infer-partial-order')
 
 function first (o) {
   for(var k in o)
     return k
 }
 
+function chars(n, ch) {
+  var s = ''
+  while(n--)
+    s += ch
+  return s
+}
+
 var create = module.exports = function (seed, cb) {
-  var rng = new RNG.MT(seed)
-  var l = 10000
+  var rng = new RNG.MT(seed), all = {}, created = [], called = []
+  var l = 10000, pending = 0
 
   var heap = [], not_called = []
   var result
@@ -21,7 +30,7 @@ var create = module.exports = function (seed, cb) {
     queued = true
     if(ended) return
 
-    rng.random() < 0.4 ? setImmediate(call) : setTimeout(call)
+    rng.random() < 0.4 ? setImmediate(call) : setTimeout(call, 10)
 
     function call() {
 
@@ -33,23 +42,41 @@ var create = module.exports = function (seed, cb) {
       if(cb) {
         try { cb() }
         catch (err) { console.error(err); return async.done(err) }
-        setImmediate(next) 
+        setImmediate(next)
      }
- 
     }
   }
 
-  function async (cb, err) {
+  function async (cb, name) {
     var m = 'cb was not called\n  created at:'
-    err = err || new Error(m)
-    err.message = m
-    not_called.push(err)
+    var id
+    name = name || new Error(m)
+
+    if(name.stack) {
+      //name.message = m
+
+      var line = name.stack.split('\n').filter(function (line) {
+        return /^\s+at /.test(line)
+      })[1].replace(/^\s+at\s/, '')
+      name = path.relative(process.cwd(), line)
+
+    }
+    all[name] = (all[name] || 0) + 1
+    id = name + '(' + all[name] + ')'
+    created.push(id)
+
+    not_called.push(id)
+
     return function () {
       var args = [].slice.call(arguments)
       var self = this
-      var called = false
+      pending ++
+      //console.log(chars(pending*2, '-')+'>', id)
       function _cb () {
-        not_called.splice(not_called.indexOf(err), 1)
+        called.push(id)
+        not_called.splice(not_called.indexOf(name), 1)
+        //console.log('<'+chars(pending*2, '-'), id)
+        pending --
         return cb.apply(self, args)
       }
 
@@ -70,11 +97,11 @@ var create = module.exports = function (seed, cb) {
     }
   }
 
-  async.through = function () {
-    var err = new Error()
+  async.through = function (name) {
+    name = name || new Error()
     return function (read) {
       return function (abort, cb) {
-        read(abort, async(cb, err))
+        read(abort, async(cb, name))
       }
     }
   }
@@ -106,9 +133,17 @@ var create = module.exports = function (seed, cb) {
       value: value,
       passed: !err,
       calls: 1,
-      seed: seed
+      seed: seed,
+      called: called
     }
 
+    console.log('created from: count')
+    console.log(all)
+    console.log('created order:')
+    console.log(created)
+    console.log('called order:')
+    console.log(called)
+    console.log('*********************')
     if(cb) cb(err, result)
     else if(err) throw err
     else console.log(result)
@@ -163,31 +198,41 @@ create.test = function (test, cb) {
 
     //collect the most common error messages
     var messages = {}
+    var called = []
 
     results.forEach(function (e) {
+      var error = e.error
       if((!e.error) && e.calls === 1)
         stats.passes ++
       else {
         if(seed == null) seed = e.seed
         if(err == null)  err = e.error
         stats.failures ++
-        if(!messages[err.message])
-          messages[err.message] = {error: err, count: 1}
-        else
-          messages[err.message].count ++
       }
+      var outcome = error ? error.message : 'passed'
+      if(!messages[outcome])
+        messages[outcome] = {error: err, count: 1, called: [e.called]}
+      else {
+        messages[outcome].count ++
+        messages[outcome].called.push(e.called)
+      }
+
       if(e.calls > 1)
         stats.errors ++
     })
 
-    var worst = Object.keys(messages)
+    var outcomes = Object.keys(messages)
                 .sort(function (a, b) {
                   return messages[b].count - messages[a].count
-                }).slice(0, 5).map(function (key) {
-                  return messages[key]
-                })
+                }).slice(0, 5)
 
-    console.log(worst)
+    var worstErrors = outcomes.map(function (key) {
+                return messages[key]
+              })
+
+    console.log(worstErrors)
+
+    console.log(infer(messages[outcomes[0]].called, true))
 
     if(stats.failures) {
       var message =
